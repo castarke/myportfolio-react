56098a941d805dcef88bd90d3c0db598fb6cfdc1
castarke <candler.starke@yahoo.com>
2023-06-15 02:58:24 -0400

    added styling to my picture on about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function About() {
   return (
     <div style={{ textAlign: 'center', marginTop: '25px', marginBottom: '75px' }}>
-      <img src="/assets/1570744259165.jpeg" alt="My Image" style={{ display: 'block', margin: '0 auto' }} />
+      <img src="/assets/1570744259165.jpeg" alt="My Image" style={{ display: 'block', margin: '0 auto', borderRadius:'50%', overflow:'hidden',boxShadow: '0 0 550px rgba(255, 215, 0, 0.3)'}} />
       <h1 style={{ marginTop: '25px' }}>About Me!</h1>
       <p className='about'>
         Junior programer who is currently enrolled in the Georgia Tech Full Stack bootcamp. Enjoying learning new things about the tech world and learning how to properly code. My hobbies outside of class and work include: firing up the smoker and bbqing, spoiling my dog, and working on cars.
